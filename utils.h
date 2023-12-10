251ddaa2a105b5dbc320101132f2750a5c88f020
Murilo Capozzi <murilocapozzi1@gmail.com>
2023-12-10 12:59:22 -0300

    Camera

---
+++ utils.h
@@ -0,0 +1,10 @@
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef CONSTANTES
+#define PI 3.14159265358979323846
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#endif
